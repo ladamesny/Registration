493ceba268bbb1d1278929e5f728646e62468354
Larry Adames <adames.larry@gmail.com>
2014-11-24 11:52:04 -0500

    First Commite of Registration and Login/Logout Project

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,34 @@
+class UsersController < ApplicationController
+  before_action :check_login, :only =>[:index]
+  def index
+
+  end
+
+  def show
+  end
+
+  def new
+    @user = User.new
+    render :layout => false
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:status] = true
+      flash[:alert] = "Eureeka! You've successfully registered..."
+    else
+      flash[:status] = false
+      flash[:alert] = @user.errors.full_messages
+    end
+    redirect_to register_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :password, :password_confirmation)
+  end
+
+  
+end
---
+++ app/models/user.rb
@@ -0,0 +1,32 @@
+class User < ActiveRecord::Base
+require 'digest/md5'
+  before_save :encrypt_password
+
+  validates :name,
+    presence: true,
+    length: { minimum: 4, allow_blank: true}
+
+  validates_uniqueness_of :name
+  
+  validates :password,
+    presence: true,
+    length: {minimum: 6, allow_blank: true},
+    confirmation: true
+
+  validates :password_confirmation,
+    presence: true
+
+  def encrypt_password
+    self.password = Digest::MD5.hexdigest(password)
+    
+  end
+
+  def self.validate_login(name,password)
+    user = User.find_by_name(name)
+    if user && user.password == Digest::MD5.hexdigest(password)
+      user
+    else
+      nil
+    end
+  end
+end
